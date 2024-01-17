78e9d59fca7662b50f2195dcce58f31fc0052434
iqragulshan <iqragulshan97@gmail.com>
2024-01-17 15:47:13 +0500

    feat: added object file

---
+++ basicJs/object/script.js
@@ -0,0 +1,17 @@
+let stdData ={
+    fName: "Iqra",
+    lName: "Gushan",
+    city: "Hunza",
+    bPlace: "hunza",
+    moreInfo:{
+        stdId: 8,
+        stdAge: 18,
+    },
+    fullName: function () {
+        return this.fName + " " + this.lName
+    }
+}
+stdData.school = "Elysian"
+delete stdData.bPlace
+console.log(stdData);
+console.log(stdData.fullName());
