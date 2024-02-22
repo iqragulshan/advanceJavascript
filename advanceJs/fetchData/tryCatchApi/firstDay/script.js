9106164fd1089b8e8931fffe553576e293870b0e
iqragulshan <iqragulshan97@gmail.com>
2024-02-22 17:22:13 +0500

    added trycatch file

---
+++ advanceJs/fetchData/tryCatchApi/firstDay/script.js
@@ -0,0 +1,54 @@
+const base_url = "https://jsonplaceholder.typicode.com/posts";
+
+async function fetchPost() {
+    const errorMsg = document.getElementById("error");
+    const loadingState = document.getElementById("Loader");
+    loadingState.style.display = "block"
+    //   console.log(response, "res here");
+    try {
+      const response = await fetch(`${base_url}`);
+      if (response.ok) {
+          const data = await response.json();
+          postData(data);
+        }
+        else {
+            console.log("failed to fetch posts");
+        }
+    } catch (error) {
+        console.log(error);
+        errorMsg.innerText =error.message
+    }
+    finally {
+      loadingState.style.display = "none"
+
+  }
+}
+
+
+function deleteCard(id) {
+    console.log(id);
+    const dltCard = document.getElementById(`post_${id}`)
+    if (dltCard) {
+        dltCard.remove()
+    }
+}
+
+function postData(data) {
+  console.log(data, "data");
+  const cardSection = document.getElementById("postContainer");
+  cardSection.innerHTML = ""
+  data.map(post => {
+      const card = document.createElement('div');
+      card.className = "card";
+      card.id = `post_${post.id}`;
+      card.innerHTML= `
+      <h2>${post.userId}</h2>
+      <h5> ${post.title}</h5>
+      <p>${post.body}</p>
+      <button onclick="deleteCard(${post.id})">delete card</button>
+      `
+      cardSection.appendChild(card)
+
+  })
+
+}
